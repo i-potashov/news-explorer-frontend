e41399440346ed02b1dfd522fb69eee6192fb6c1
i-potashov <i.ptshv@gmail.com>
2020-05-31 18:03:55 +0300

    Add fetch

--- src/js/constants/news-api-config.js
+++ src/js/constants/news-api-config.js
@@ -1,10 +1,11 @@
 export default {
-  // SERVER_URL: 'https://praktikum.tk/news/v2/everything',
-  SERVER_URL: 'https://newsapi.org/v2/everything',
+  SERVER_URL: 'https://praktikum.tk/news/v2/everything',
   KEY: '1c98d96ded7a45c58e8435ca81fa9fb8',
-  // REQUEST_NAME: 'q',
   REQUEST_NAME: 'qInTitle',
   LANGUAGE: 'ru',
   NEWS_COUNT: '100',
   SORT_BY: 'publishedAt',
 };
+
+// SERVER_URL: 'https://newsapi.org/v2/everything',
+// REQUEST_NAME: 'q',
