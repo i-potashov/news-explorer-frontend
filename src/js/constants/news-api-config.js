25c2ab1e59c2b901f1266dceb76e08e261e6bcd1
i-potashov <i.ptshv@gmail.com>
2020-05-31 17:45:01 +0300

    Add proxy

--- src/js/constants/news-api-config.js
+++ src/js/constants/news-api-config.js
@@ -1,5 +1,6 @@
 export default {
-  SERVER_URL: 'https://praktikum.tk/news/v2/everything',
+  // SERVER_URL: 'https://praktikum.tk/news/v2/everything',
+  SERVER_URL: 'https://newsapi.org/v2/everything',
   KEY: '1c98d96ded7a45c58e8435ca81fa9fb8',
   // REQUEST_NAME: 'q',
   REQUEST_NAME: 'qInTitle',
